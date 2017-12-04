be7f4917888ca1f81a9c7bf571ef677f0df373b6
Basia <barbara.szczgielska@gmail.com>
2017-12-04 13:01:16 +0100

    initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',  socket => {
+  console.log('a user connected');
+});
+
+http.listen(port, () => {
+  console.log('listening on: ' + port);
+});
---
+++ simulation/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: 'http://localhost:4000'
+    };
+  }
+  
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        Symulacja
+      </div>
+    );
+  }
+}
+export default App;
