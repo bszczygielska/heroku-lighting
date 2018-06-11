daf2e2082f524b61d19d74f5d97bc4180d897863
Basia <barbara.szczgielska@gmail.com>
2018-06-11 23:51:13 +0200

    scene name form

---
+++ client/src/app/components/lightScenes/AddSceneName.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Button, Input, Form } from 'antd';
+import { ClientStore } from '../../stores/ClientStore';
+import { observer } from 'mobx-react';
+
+const FormItem = Form.Item;
+
+interface IAddLightBulbProps {
+  form: any;
+  clientStore: ClientStore;
+}
+
+@observer
+class AddSceneName extends React.Component<IAddLightBulbProps, any> {
+
+  private handleSubmit = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    this.props.form.validateFields((err: any, values: any) => {
+      if (err) {
+        console.log(err.message)
+      }
+      else {
+        this.props.clientStore.setValue('sceneName', values.sceneName)
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form layout="inline" onSubmit={ this.handleSubmit }>
+          <FormItem label="Scene name">
+            { getFieldDecorator('sceneName', {
+              rules: [{ required: true, message: 'Please input scene name!' }],
+            })(<Input placeholder="ex relaxing mood"/>) }
+          </FormItem>
+        <FormItem>
+          <Button type="primary"
+                  htmlType="submit">Save scene</Button>
+        </FormItem>
+      </Form>
+
+    );
+  }
+}
+
+const AddSceneNameForm = Form.create()(AddSceneName);
+export default AddSceneNameForm;
