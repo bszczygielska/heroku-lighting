b68c858b300fce121ac901262d85f696fc66e4eb
bsz <barbara.szczgieska@gmail.com>
2018-09-17 20:41:02 +0200

    try

--- server/app.js
+++ server/app.js
@@ -7,8 +7,10 @@ const path = require('path');
 
 const port = process.env.PORT || 5000;
 
+
 try {
   app.use(express.static('../client/build'));
+
 
   http.listen(port, () => {
     console.log('io server listening on: ' + port);
@@ -132,7 +134,7 @@ try {
    */
 
   app.get('*', (req, res) => {
-    res.sendFile('../client/build/index.html');
+    res.sendFile(path.resolve(__dirname + '../client/build/index.html'));
   });
 
   app.get('/lights', function(req, res) {
